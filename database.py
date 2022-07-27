dffcde7c74d42fbb0fdddbeebd6ee0517c9b8aa9
victor tobi <victortobi157@gmail.com>
2022-07-27 23:04:45 +0100

    databasesql

--- database.py
+++ database.py
@@ -8,4 +8,36 @@ import sqlite3
 def connect():
     conn = sqlite3.connect('books.db')
     cursor = conn.cursor()
-    cursor.execute('')
+    cursor.execute('CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year integer, book_number integer)')
+    conn.commit()
+    conn.close()
+
+
+def insert(title, author,year,book_number):
+    conn = sqlite3.connect('books.db')
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO book VALUES (NULL,?,?,?,?)", (title,author,year,book_number))
+    conn.commit()
+    conn.close()
+
+
+def view():
+    conn = sqlite3.connect('books.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM book")
+    rows = cursor.fetchall()
+    conn.commit()
+    conn.close()
+    return rows
+
+def search(title,author,year,book_number):
+    conn = sqlite3.connect('books.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR book_number=?")
+    rows = cursor.fetchall()
+    conn.commit()
+    conn.close()
+    return rows
+
+connect()
+insert()
